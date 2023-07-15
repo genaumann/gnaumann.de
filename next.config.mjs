1512108f5d258ce1ba7e58395aa55f1495935078
Gino Naumann <gino@nauminator.de>
2023-07-16 01:43:44 +0200

    chore: install and configure remark-frontmatter

--- next.config.mjs
+++ next.config.mjs
@@ -1,19 +1,20 @@
-import remarkgfm from "remark-gfm";
-import nextMDX from "@next/mdx";
+import remarkgfm from 'remark-gfm'
+import frontmatter from 'remark-frontmatter'
+import nextMDX from '@next/mdx'
 
 const withMDX = nextMDX({
   extension: /\.mdx?$/,
   options: {
-    remarkPlugins: [remarkgfm],
-    rehypePlugins: [],
-  },
-});
+    remarkPlugins: [remarkgfm, frontmatter],
+    rehypePlugins: []
+  }
+})
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "standalone",
+  output: 'standalone',
   reactStrictMode: true,
-  pageExtensions: ["ts", "tsx", "js", "jsx", "md", "mdx"],
-};
+  pageExtensions: ['ts', 'tsx', 'js', 'jsx', 'md', 'mdx']
+}
 
-export default withMDX(nextConfig);
+export default withMDX(nextConfig)
