8031a7727e16d0b3f600e19c9b253fe44381df1a
Gino Naumann <gino@nauminator.de>
2023-07-15 17:18:38 +0200

    chore: add formatDate util

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -16,3 +16,11 @@ export const calcAge = (birthdate: Date): number => {
 
   return age
 }
+
+export const formatDate = (dateString: string) => {
+  const date = new Date(dateString)
+  const day = date.getDate().toString().padStart(2, '0')
+  const month = (date.getMonth() + 1).toString().padStart(2, '0')
+  const year = date.getFullYear()
+  return `${day}.${month}.${year}`
+}
