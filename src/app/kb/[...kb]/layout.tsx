f5fc9b0e029006bd814b32d7bfbbfeac3327e8eb
Gino Naumann <gino@nauminator.de>
2023-07-28 00:30:00 +0200

    chore: delete deprecated search button

--- src/app/kb/[...kb]/layout.tsx
+++ src/app/kb/[...kb]/layout.tsx
@@ -2,9 +2,9 @@ import KBMobileNav from '@/components/layout/kb/KBMobileNav'
 import {KBLayoutProps} from '@/types'
 import KBIndex from '@/KBIndex.json'
 import KBNav from '@/components/layout/kb/KBNav'
-import KBNavSearch from '@/components/layout/kb/KBNavSearch'
 import {Metadata} from 'next'
 import {findArticleByHref} from '@/utils'
+import SearchButton from '@/components/SearchButton'
 
 export const generateMetadata = async ({
   params
@@ -25,7 +25,7 @@ const KBLayout = ({children}: KBLayoutProps) => {
       </div>
       <div className="md:flex gap-x-10">
         <div className="hidden md:block min-w-[224px] max-w-[224px]">
-          <KBNavSearch />
+          <SearchButton className="py-2 mt-3" location="body" />
           <KBNav index={KBIndex} />
         </div>
         <article className="grow mt-3 prose">{children}</article>
