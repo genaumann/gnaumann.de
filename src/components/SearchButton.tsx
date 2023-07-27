12927f9f3c6fe113fce40347d89a22ce71e2a77c
Gino Naumann <gino@nauminator.de>
2023-07-27 23:42:51 +0200

    refactor(page): kb ssr

--- src/components/SearchButton.tsx
+++ src/components/SearchButton.tsx
@@ -1,18 +1,32 @@
+'use client'
+
 import {faSearch} from '@fortawesome/pro-regular-svg-icons'
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
 import clsx from 'clsx'
+import Modal from './Modal'
+import {useState} from 'react'
 
 const SearchButton: React.FC<React.ButtonHTMLAttributes<HTMLButtonElement>> = ({
   className,
   ...props
 }) => {
+  const [isOpen, setIsOpen] = useState(false)
   return (
-    <button
-      {...props}
-      className={clsx('bg-gray-200 min-w-full rounded-xl', className)}>
-      <FontAwesomeIcon className="mr-3" icon={faSearch} fixedWidth />
-      Suchen
-    </button>
+    <>
+      <button
+        {...props}
+        onClick={() => setIsOpen(!isOpen)}
+        className={clsx('bg-gray-200 min-w-full rounded-xl', className)}>
+        <FontAwesomeIcon className="mr-3" icon={faSearch} fixedWidth />
+        Suchen
+      </button>
+      <Modal
+        isOpen={isOpen}
+        setIsOpen={setIsOpen}
+        title="Die Suche ist noch nicht verfügbar"
+        description="Docsearch kommt bald 🚀"
+      />
+    </>
   )
 }
 
---
+++ src/components/modules/kb/KBIndexTree.tsx
@@ -0,0 +1,92 @@
+'use client'
+
+import {KBIndex} from '@/types'
+import {
+  faBookOpenReader,
+  faChevronDown,
+  faChevronRight
+} from '@fortawesome/pro-duotone-svg-icons'
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
+import clsx from 'clsx'
+import Link from 'next/link'
+import {useEffect, useState} from 'react'
+import navigation from '@/KBIndex.json'
+import styles from '@/styles/kbindex.module.css'
+
+const KBIndexNode = ({node}: {node: KBIndex}) => {
+  const [icon, setIcon] = useState<JSX.Element>()
+  const [isOpen, setIsOpen] = useState(true)
+
+  const handleToggle = (event: React.SyntheticEvent<HTMLDetailsElement>) => {
+    setIsOpen(event.currentTarget.open)
+  }
+
+  useEffect(() => {
+    if (isOpen) {
+      setIcon(
+        <FontAwesomeIcon
+          className="float-right"
+          icon={faChevronDown}
+          fixedWidth
+        />
+      )
+    } else {
+      setIcon(
+        <FontAwesomeIcon
+          className="float-right"
+          icon={faChevronRight}
+          fixedWidth
+        />
+      )
+    }
+  }, [isOpen])
+
+  return (
+    <li className="text-xl">
+      <details open={isOpen} onToggle={handleToggle}>
+        <summary>
+          <Link
+            className={clsx(node.children && 'font-bold', 'hover:text-primary')}
+            href={node.href}>
+            {node.title}
+          </Link>
+          {node.children && icon}
+        </summary>
+        {node.children && (
+          <ul className="space-y-4">
+            {node.children.map((childNode, index) => (
+              <KBIndexNode key={index} node={childNode} />
+            ))}
+          </ul>
+        )}
+      </details>
+    </li>
+  )
+}
+
+const KBIndexTree = () => {
+  return (
+    <nav className="mt-3 md:flex-grow max-w-[734px] md:mr-0 md:ml-auto">
+      <ul role="list" className={clsx(styles.tree)}>
+        <li className="!pl-3">
+          <details open>
+            <summary
+              onClick={e => e.preventDefault()}
+              className="text-3xl font-bold">
+              <FontAwesomeIcon icon={faBookOpenReader} fixedWidth />
+            </summary>
+            <ul
+              role="list"
+              className={clsx('bg-gray-200 rounded-xl space-y-4 mt-4 p-4')}>
+              {navigation.map((node, index) => (
+                <KBIndexNode key={index} node={node} />
+              ))}
+            </ul>
+          </details>
+        </li>
+      </ul>
+    </nav>
+  )
+}
+
+export default KBIndexTree
