38b8cfe6a8a7c56644770f8dea5cca8584d01e9d
Gino Naumann <gino@nauminator.de>
2023-07-31 21:24:59 +0200

    feat(mdx): add highlight code rehype plugin

---
+++ src/rehype/highlightCode.ts
@@ -0,0 +1,140 @@
+import {Plugin} from 'unified'
+import {Root, ElementContent, Element} from 'hast'
+import {visit} from 'unist-util-visit'
+import {Node} from 'unist'
+import {lowlight} from 'lowlight'
+import {toString} from 'hast-util-to-string'
+
+type markMapType = {
+  [index: string]: {marker: string; class: string}
+}
+
+const markLines = (input: string) => {
+  const lines = input.split('\n')
+
+  const markMap: markMapType = {
+    add: {marker: '>>>>>>', class: 'bg-teal-900/50'},
+    del: {marker: '<<<<<<', class: 'bg-red-900/50'}
+  }
+
+  const markedLines: ElementContent[] = lines.map(line => {
+    let markType = undefined
+    if (line.startsWith(markMap['add'].marker)) {
+      markType = 'add'
+    }
+    if (line.startsWith(markMap['del'].marker)) {
+      markType = 'del'
+    }
+    if (markType !== undefined) {
+      const text = line.replace(markMap[markType].marker, '')
+      return {
+        type: 'element',
+        tagName: 'span',
+        properties: {
+          className: [markMap[markType].class, 'hlLine']
+        },
+        children: [
+          {
+            type: 'text',
+            value: `${text}\n`
+          }
+        ]
+      }
+    } else {
+      return {
+        type: 'text',
+        value: `${line}\n`
+      }
+    }
+  })
+  return markedLines
+}
+
+const language = (node: Element) => {
+  const className = node.properties && node.properties.className
+  let index = -1
+
+  if (!Array.isArray(className)) {
+    return
+  }
+
+  while (++index < className.length) {
+    const value = String(className[index])
+
+    if (
+      value === 'no-highlight' ||
+      value === 'nohighlight' ||
+      value === 'language-nohighlight'
+    ) {
+      return false
+    }
+
+    if (value.slice(0, 5) === 'lang-') {
+      return value.slice(5)
+    }
+
+    if (value.slice(0, 9) === 'language-') {
+      return value.slice(9)
+    }
+  }
+}
+
+const highlightLine: Plugin<void[], Root> = () => {
+  return tree => {
+    visit(tree, 'element', (node, _, givenParent) => {
+      const parent = givenParent as Node
+      const name = 'hljs'
+
+      if (
+        !parent ||
+        !('tagName' in parent) ||
+        parent.tagName !== 'pre' ||
+        node.tagName !== 'code' ||
+        node.children.length !== 1 ||
+        !('value' in node.children[0]) ||
+        !node.properties
+      ) {
+        return
+      }
+
+      const lang = language(node)
+
+      if (lang === false) {
+        return
+      }
+
+      if (!Array.isArray(node.properties.className)) {
+        node.properties.className = []
+      }
+
+      if (!node.properties.className.includes(name)) {
+        node.properties.className.unshift(name)
+      }
+
+      const markedLines = markLines(node.children[0].value)
+
+      const allChildren = markedLines.map(element => {
+        const res = lang
+          ? lowlight.highlight(lang, toString(element).replace('\n', ''))
+          : lowlight.highlightAuto(toString(element).replace('\n', ''))
+        if ('children' in element && res.children.length > 0) {
+          element.children = res.children
+          return element
+        }
+        if (
+          ('value' in element && element.value === '\n') ||
+          element.type === 'element'
+        ) {
+          return element
+        }
+        if ('value' in element && element.value.endsWith('\n')) {
+          res.children.push({type: 'text', value: '\n'})
+        }
+        return res.children
+      })
+      node.children = allChildren.flat()
+    })
+  }
+}
+
+export default highlightLine
