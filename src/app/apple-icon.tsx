a9b07e9c91f5d262e293dbfdbbd085b38d559186
Gino Naumann <gino@nauminator.de>
2023-08-02 01:32:46 +0200

    feat(seo): generate apple-icon

---
+++ src/app/apple-icon.tsx
@@ -0,0 +1,32 @@
+import {basePath} from '@/config'
+import {ImageResponse} from 'next/server'
+
+export const runtime = 'edge'
+
+export const size = {
+  width: 180,
+  height: 180
+}
+export const contentType = 'image/png'
+
+const Icon = () => {
+  return new ImageResponse(
+    (
+      <div tw="flex bg-slate-900 w-full h-full relative text-white justify-center">
+        <div tw="flex items-center">
+          <img
+            src={`${basePath}/gnaumann_col_cut.png`}
+            alt="Logo"
+            height={150}
+            width={110}
+          />
+        </div>
+      </div>
+    ),
+    {
+      ...size
+    }
+  )
+}
+
+export default Icon
