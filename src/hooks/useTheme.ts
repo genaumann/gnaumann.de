2a6e45a5ad2b587fbb1d4213aa47ed69049e9c19
Gino Naumann <gino@nauminator.de>
2023-08-11 01:35:52 +0200

    feat(hook): add useTheme

---
+++ src/hooks/useTheme.ts
@@ -0,0 +1,43 @@
+'use client'
+
+import {useAppSelector} from '@/redux/store'
+import {Theme} from '@/types'
+import {useEffect, useState} from 'react'
+
+const getOsMode = () => {
+  return window.matchMedia &&
+    window.matchMedia('(prefers-color-scheme: dark)').matches
+    ? Theme.DARK
+    : Theme.LIGHT
+}
+
+const useTheme = () => {
+  const themeState = useAppSelector(state => state.AppTheme.theme)
+  const [osMode, setOsMode] = useState<Theme>(getOsMode())
+  const [theme, setTheme] = useState<Theme>(Theme.DARK)
+
+  useEffect(() => {
+    setTheme(themeState === Theme.AUTO ? osMode : themeState)
+  }, [themeState])
+
+  useEffect(() => {
+    if (theme === 'dark') {
+      document.documentElement.classList.add('dark')
+      document.documentElement.classList.remove('light')
+    } else {
+      document.documentElement.classList.remove('dark')
+      document.documentElement.classList.add('light')
+    }
+  }, [theme])
+
+  useEffect(() => {
+    const handler = () => setOsMode(getOsMode())
+    const e = window.matchMedia('(prefers-color-scheme: dark)')
+    e.addEventListener('change', handler)
+    return () => e.removeEventListener('change', handler)
+  }, [])
+
+  return {theme, themeState}
+}
+
+export default useTheme
