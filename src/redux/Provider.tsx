2860b9f913270487bdef2c86739c53d93071426f
Gino Naumann <gino@nauminator.de>
2023-08-11 22:05:00 +0200

    chore(redux): add localstorage support

--- src/redux/Provider.tsx
+++ src/redux/Provider.tsx
@@ -1,12 +1,17 @@
 'use client'
 
 import {Provider} from 'react-redux'
-import {reduxStore} from './store'
+import {reduxStore, persistor} from './store'
 import {ChildProps} from '@/types'
+import {PersistGate} from 'redux-persist/integration/react'
 
 const ReduxProvider = ({children}: ChildProps) => {
   const store = reduxStore
-  return <Provider store={store}>{children}</Provider>
+  return (
+    <Provider store={store}>
+      <PersistGate persistor={persistor}>{children}</PersistGate>
+    </Provider>
+  )
 }
 
 export default ReduxProvider
